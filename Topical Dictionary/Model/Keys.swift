c78e370883cb14faf12d4e77aee84df0d91f21b6
ibrahimethem <ibrhmkarali@gmail.com>
2021-06-12 19:10:18 +0300

    add keys

---
+++ Topical Dictionary/Model/Keys.swift
@@ -0,0 +1,13 @@
+//
+//  Keys.swift
+//  Topical Dictionary
+//
+//  Created by İbrahim Ethem Karalı on 12.06.2021.
+//  Copyright © 2021 İbrahim Ethem Karalı. All rights reserved.
+//
+
+import Foundation
+
+enum Keys: String {
+    case dictionaryCollectionID = "example"
+}
