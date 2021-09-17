37592e7b1156f9b35915e8443dd5b3dd770a57ee
ibrahimethem <ibrhmkarali@gmail.com>
2021-09-18 01:20:16 +0300

    Add Fastlane and snapshot function

--- Podfile
+++ Podfile
@@ -1,27 +1,29 @@
 # Uncomment the next line to define a global platform for your project
 platform :ios, '13.0'
 
-#post_install do |installer|
-#    installer.pods_project.targets.each do |target|
-#        target.build_configurations.each do |config|
-#            config.build_settings['GCC_WARN_INHIBIT_ALL_WARNINGS'] = "YES"
-#        end
-#    end
-#end
-
 target 'Topical Dictionary' do
   # Comment the next line if you don't want to use dynamic frameworks
   use_frameworks!
 
   # Pods for Topical Dictionary
-
   pod 'Firebase/Auth'
   pod 'Firebase/Firestore'
   pod 'Firebase/Analytics'
   pod 'FirebaseFirestoreSwift'
   pod 'Google-Mobile-Ads-SDK'
-  pod 'FBSDKLoginKit'
-  pod 'GoogleSignIn'
+  pod 'FBSDKLoginKit', '~> 11.0.0'
+  pod 'GoogleSignIn', '~> 5.0.2'
+  pod 'GTMSessionFetcher', '~> 1.5.0'
   pod 'IQKeyboardManagerSwift'
+  pod 'ProgressHUD'
+
+  target 'Topical DictionaryTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'Topical DictionaryUITests' do
+    # Pods for testing
+  end
 
 end
--- Topical DictionaryUITests/Topical_DictionaryUITests.swift
+++ Topical DictionaryUITests/Topical_DictionaryUITests.swift
@@ -26,10 +26,76 @@ class Topical_DictionaryUITests: XCTestCase {
     func testExample() throws {
         // UI tests must launch the application that they test.
         let app = XCUIApplication()
-        app.launch()
 
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        snapshot("01LoginScreen")
+        
+        let button = app.buttons["Continue with E-mail"]
+        if button.waitForExistence(timeout: 30) {
+            button.tap()
+        }
+        
+        let signUp = app.scrollViews.otherElements.buttons["Sign in"]
+        if signUp.waitForExistence(timeout: 30) {
+            signUp.tap()
+        }
+        
+        let elementsQuery = XCUIApplication().scrollViews.otherElements
+        
+        let email = elementsQuery.textFields["Email"]
+        email.tap()
+        email.typeText(testEmail)
+        
+        let password = elementsQuery.secureTextFields["Password"]
+        password.tap()
+        password.typeText(testPassword)
+        
+        
+        
+        let signInButton = elementsQuery.buttons["Sign in with Email"]
+        if signInButton.waitForExistence(timeout: 30) {
+            signInButton.tap()
+        }
+        
+        let app2 = XCUIApplication()
+
+        let cell = app2.tables.cells.staticTexts["Cooking Instruction Dictionary"]
+        
+        if cell.waitForExistence(timeout: .init(30)) {
+            snapshot("02HomeScreen")
+            cell.tap()
+        }
+        snapshot("03DictionaryScreen")
+        
+        let tabbar = app2.tabBars["Tab Bar"]
+        tabbar.children(matching: .button).element(boundBy: 2).tap()
+        snapshot("04AccountScreen")
+        
+        let settings = app2.tables.cells.staticTexts["Settings"]
+        if settings.waitForExistence(timeout: 10) {
+            settings.tap()
+        }
+        
+        let defaultSwitch = app2.tables.cells.switches["deviceDefaultSwitch"]
+        
+        if defaultSwitch.waitForExistence(timeout: 10) {
+            defaultSwitch.swipeLeft()
+        }
+        
+        let lightTheme = app2.tables.cells.staticTexts["Dark"]
+        
+        if lightTheme.waitForExistence(timeout: 10) {
+            lightTheme.tap()
+        }
+        
+        let accountTab = tabbar.children(matching: .button).element(boundBy: 0)
+        accountTab.tap()
+        accountTab.tap()
+        
+        snapshot("05DarkMode")
+        
     }
 
     func testLaunchPerformance() throws {
@@ -39,5 +105,13 @@ class Topical_DictionaryUITests: XCTestCase {
                 XCUIApplication().launch()
             }
         }
+    }
+    
+    override class func setUp() {
+        super.setUp()
+        
+        let app = XCUIApplication()
+        setupSnapshot(app)
+        app.launch()
     }
 }
